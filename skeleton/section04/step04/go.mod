597393dec3d371239424792c9593715ff5528e1d
makoto hata <makotohata0429@gmail.com>
2024-06-23 11:01:34 +0900

    section04

--- skeleton/section04/step04/go.mod
+++ skeleton/section04/step04/go.mod
@@ -1,3 +1,5 @@
-module github.com/gohandson/gacha-ja
+module github.com/makoto00000/go-gacha/skeleton/section04
 
-go 1.15
+go 1.22.4
+
+require github.com/makoto00000/go-gacha v0.0.3
--- skeleton/section04/step04/main.go
+++ skeleton/section04/step04/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	// TODO: インポートパスを公開したものに変更する
-	"github.com/gohandson/tenntenn/gacha"
+	"github.com/makoto00000/go-gacha/skeleton/section04/gacha"
 )
 
 func main() {
